f97e57b1818fdc23b45793aeb05518efd0f1e85b
Myoungjun Ko <kmj951015@gmail.com>
2025-03-21 00:47:08 +0900

    :sparkles: 리프레시 토큰 및 로그아웃 기능 추가

--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -1,12 +1,18 @@
 from fastapi import APIRouter, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
+from redis.asyncio import Redis
 
-from app.api.dependencies import get_db
+from app.api.dependencies import get_db, get_redis
 from app.core.exceptions import NotAcceptableException
 from app.crud.user import create_user_in_db, get_user_in_db, update_user_in_db
-from app.schemas.user import UserCreate, Token, UserUpdate
-from app.core.security import verify_user, get_current_user_in_db
+from app.schemas.user import UserCreate, AccessToken, UserUpdate, RefreshToken
+from app.core.security import (
+    issue_access_token,
+    reissue_access_token,
+    delete_token,
+    get_current_user_in_db,
+)
 from app.services.user import is_existing_user, is_not_existing_user
 from app.models.user import User
 
@@ -17,26 +23,52 @@ router = APIRouter()
 async def create_user(_user_info: UserCreate, db: AsyncSession = Depends(get_db)):
     """
     사용자를 생성하는 API
+    Args:
+        _user_info: 생성할 사용자의 정보
+        db: AsyncSession
     """
     if _user_info.name.lower() == "all":
         raise NotAcceptableException(detail="Could not use this name")
-    await is_not_existing_user(db, name=_user_info.name, email=_user_info.email)
+    await is_not_existing_user(db=db, name=_user_info.name, email=_user_info.email)
     await create_user_in_db(db=db, user_info=_user_info)
 
 
-@router.post("/login", response_model=Token)
-async def login_for_access_token(
-    form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)
+@router.post("/login", response_model=AccessToken)
+async def login(
+    db: AsyncSession = Depends(get_db),
+    redis_db: Redis = Depends(get_redis),
+    form_data: OAuth2PasswordRequestForm = Depends(),
 ):
     """
     로그인을 위한 액세스 토큰을 발급하는 API
+    Args:
+        db: AsyncSession
+        redis_db: redis db
+        form_data: 요청 하는 자료
+
+    Returns:
+        액세스 토큰
     """
-    return await verify_user(form_data, db)
+    return await issue_access_token(db=db, redis_db=redis_db, form_data=form_data)
+
+
+@router.post("/refresh", response_model=RefreshToken)
+async def refresh(refresh_token: str, redis_db: Redis = Depends(get_redis)):
+    """
+    리프레시 토큰을 이용해 새로운 액세스 토큰을 발급하는 API
+    Args:
+        refresh_token: 기존 리프레시 토큰
+        redis_db: redis db
+
+    Returns:
+        새로운 액세스 토큰
+    """
+    return await reissue_access_token(refresh_token=refresh_token, redis_db=redis_db)
 
 
 @router.post("/logout", status_code=status.HTTP_204_NO_CONTENT)
-async def logout_for_access_token(db: AsyncSession = Depends(get_db)):
-    pass
+async def logout(redis_db: Redis = Depends(get_redis)):
+    return await delete_token(redis_db=redis_db)
 
 
 @router.get("/list/me", status_code=status.HTTP_200_OK)
@@ -45,6 +77,11 @@ async def get_current_user(
 ):
     """
     현재 로그인한 사용자 정보를 반환하는 API
+    Args:
+        current_user: 현재 사용자 정보
+
+    Returns:
+        현재 사용자의 정보
     """
     return current_user
 
@@ -52,7 +89,13 @@ async def get_current_user(
 @router.get("/list/{user_name}", status_code=status.HTTP_200_OK)
 async def get_user(user_name: str, db: AsyncSession = Depends(get_db)):
     """
-    특정 사용자 정보를 반환하는 API
+    특정 사용자 정보를 반환 하는 API
+    Args:
+        user_name: 사용자의 이름
+        db: AsyncSession
+
+    Returns:
+        찾고자 하는 사용자의 정보
     """
     user = await is_existing_user(db, name=user_name)
     return user
@@ -62,7 +105,12 @@ async def get_user(user_name: str, db: AsyncSession = Depends(get_db)):
 @router.get("/list", status_code=status.HTTP_200_OK)
 async def get_all_user(db: AsyncSession = Depends(get_db)):
     """
-    사용자 전체 정보를 반환하는 API
+    사용자 전체 정보를 반환 하는 API
+    Args:
+        db: AsyncSession
+
+    Returns:
+        db에 있는 모든 사용자의 정보
     """
     return await get_user_in_db(db)
 
@@ -75,6 +123,13 @@ async def update_user(
 ):
     """
     사용자 정보를 수정하는 API
+    Args:
+        user_update: 갱신할 사용자의 정보
+        current_user: 현재 사용자
+        db: AsyncSession
+
+    Returns:
+        갱신된 사용자의 정보
     """
     # 업데이트할 데이터 필터링 (None이 아닌 값만 업데이트)
     update_data = user_update.model_dump(exclude_unset=True)
--- app/core/security.py
+++ app/core/security.py
@@ -5,11 +5,13 @@ from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from passlib.context import CryptContext
 from sqlalchemy.ext.asyncio import AsyncSession
+from redis.asyncio import Redis
 
 from app.config import settings
 from app.core.exceptions import CredentialsException
-from app.api.dependencies import get_db
+from app.api.dependencies import get_db, get_redis
 
+# token 부분만 추출
 oauth2_scheme = OAuth2PasswordBearer(
     tokenUrl=f"/api/{settings.API_VERSION}/users/login"
 )
@@ -18,49 +20,160 @@ oauth2_scheme = OAuth2PasswordBearer(
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-async def verify_user(
-    form_data: OAuth2PasswordRequestForm = Depends(), db: AsyncSession = Depends(get_db)
+async def create_token(expire_time: int, user_name: str):
+    """
+    token을 만드는 함수
+    Args:
+        expire_time: 토큰 유효 시간 (분 단위)
+        user_name: 사용자 이름
+
+    Returns:
+        만든 토큰
+    """
+    utc_now = datetime.now(timezone.utc)
+
+    exp = utc_now + timedelta(minutes=expire_time)
+    payload = {"sub": user_name, "exp": exp.timestamp()}
+    token = jwt.encode(
+        payload=payload, key=settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    )
+
+    return token
+
+
+async def decode_token(token: str, detail: str = "Invalid token") -> (str, float):
+    """
+    토큰을 복호화하여 정상적인 토큰인지 확인하는 함수
+    Args:
+        token: access or refresh token
+        detail: 유효 하지 않을 때, 출력될 에러 메시지
+
+    Returns:
+        토큰 주인의 이름, 토큰 만료 기간
+    """
+    payload = jwt.decode(
+        jwt=token,
+        key=settings.SECRET_KEY,
+        algorithms=[settings.ALGORITHM],
+        options={"verify_exp": True},
+    )
+    username: str = payload.get("sub")
+    if username is None:
+        raise CredentialsException(detail=detail)
+
+    return username, payload["exp"]
+
+
+async def issue_access_token(
+    db: AsyncSession, redis_db: Redis, form_data: OAuth2PasswordRequestForm = Depends()
 ):
     """
-    해당 사용자가 인증된 사용자인지 확인하는 함수
+    해당 사용자가 인증된 사용자인지 확인 후, 액세스 토큰과 리프레시 토큰을 발급하는 함수
     Args:
-        form_data: 현재 요청받은 데이터
         db: AsyncSession
+        redis_db: redis db
+        form_data: 현재 요청 받은 데이터
 
     Returns:
         액세스 토큰 발급
     """
     from app.crud.user import get_user_in_db  # 순환 참조를 막기 위한 지연 참조
 
-    user = await get_user_in_db(db, form_data.username)
     # 암호화되지 않은 비밀번호를 암호화하여 데이터베이스와 일치하는지 확인하는 함수
+    user = await get_user_in_db(db, form_data.username)
     if not user or not pwd_context.verify(form_data.password, user.password):
         raise CredentialsException()
 
-    # make access token
-    utc_now = datetime.now(timezone.utc)
-    exp_time = utc_now + timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
-    payload = {"sub": user.name, "exp": exp_time.timestamp()}  # UNIX 타임스탬프로 변환
-    access_token = jwt.encode(
-        payload=payload, key=settings.SECRET_KEY, algorithm=settings.ALGORITHM
+    # make access & refresh token
+    access_token = await create_token(
+        expire_time=settings.ACCESS_TOKEN_EXPIRE_MINUTES, user_name=user.name
+    )
+    refresh_token = await create_token(
+        expire_time=settings.REFRESH_TOKEN_EXPIRE_MINUTES, user_name=user.name
+    )
+
+    # save refresh token in redis
+    await redis_db.setex(
+        name=f"refresh:{user.name}",
+        time=settings.REFRESH_TOKEN_EXPIRE_MINUTES * 60,
+        value=refresh_token,
     )
 
     return {
         "access_token": access_token,
+        "refresh_token": refresh_token,
         "token_type": "bearer",
         "name": user.name,
     }
 
 
+async def reissue_access_token(refresh_token: str, redis_db: Redis):
+    """
+    리프레시 토큰을 사용해 다시 액세스 토큰을 발급 하는 함수
+    Args:
+        refresh_token: 요청 받은 리프레시 토큰
+        redis_db: redis db
+
+    Returns:
+        새로운 액세스 토큰
+    """
+    try:
+        username, _ = await decode_token(
+            token=refresh_token, detail="Invalid refresh token"
+        )
+
+        # refresh token 확인
+        stored_refresh_token = await redis_db.get(f"refresh:{username}")
+        if stored_refresh_token != refresh_token:
+            raise CredentialsException(detail="Invalid refresh token")
+
+        # 새로운 액세스 토큰 발급
+        new_access_token = await create_token(
+            expire_time=settings.ACCESS_TOKEN_EXPIRE_MINUTES, user_name=username
+        )
+
+        return {"access_token": new_access_token, "token_type": "bearer"}
+    except jwt.ExpiredSignatureError:
+        raise CredentialsException(detail="Refresh token has expired")
+    except jwt.InvalidTokenError:
+        raise CredentialsException(detail="Invalid refresh token")
+
+
+async def delete_token(redis_db: Redis, token: str = Depends(oauth2_scheme)):
+    """
+    액세스 토큰을 무효화하고, 리프레시 토큰을 삭제하는 함수
+    Args:
+        redis_db: redis db
+        token:
+
+    Returns:
+
+    """
+    username, exp = await decode_token(token=token)
+
+    try:
+        await redis_db.delete(f"refresh:{username}")
+        remaining_time = exp - datetime.now(timezone.utc).timestamp()
+        if remaining_time > 0:
+            await redis_db.setex(f"blacklist:{token}", remaining_time, "true")
+
+        return {"message": "Successfully logged out"}
+    except jwt.InvalidTokenError:
+        raise CredentialsException(detail="Invalid token")
+
+
 # 매개변수로 사용한 토큰값은 OAuth2PasswordBearer에 의해 자동으로 매핑된다.
 async def get_current_user_in_db(
-    token: str = Depends(oauth2_scheme), db: AsyncSession = Depends(get_db)
+    token: str = Depends(oauth2_scheme),
+    db: AsyncSession = Depends(get_db),
+    redis_db: Redis = Depends(get_redis),
 ):
     """
     토큰값을 사용하여 현재 사용자 정보를 반환하는 함수
     Args:
         token: 헤더에 저장된 토큰값
         db: AsyncSession
+        redis_db: redis db
 
     Returns:
         현재 사용자의 정보
@@ -68,18 +181,19 @@ async def get_current_user_in_db(
     from app.crud.user import get_user_in_db  # 순환 참조를 막기 위한 지연 참조
 
     try:
+        # 이미 무효화 된 토큰인지 확인
+        if await redis_db.get(f"blacklist:{token}"):
+            raise CredentialsException("Token has been revoked")
+
         # 토큰을 복호화하여 토큰에 담겨 있는 사용자명을 얻기 위함
-        payload = jwt.decode(
-            jwt=token, key=settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        username, _ = await decode_token(
+            token=token, detail="Invalid authentication credentials: username not found"
         )
-        username: str = payload.get("sub")
-        if username is None:
-            raise CredentialsException()
-    except (
-        jwt.ExpiredSignatureError
-    ):  # 사용자명이 없거나 사용자명으로 데이터 조회에 실패한 경우
-        raise CredentialsException()
-    else:
+    except jwt.ExpiredSignatureError:
+        raise CredentialsException(detail="Token has expired")
+    except jwt.InvalidTokenError:
+        raise CredentialsException(detail="Invalid token")
+    else:  # db 동작 중 발생하는 에러와 jwt 에러를 구분하기 위해 else문 사용
         user = await get_user_in_db(db, name=username)
         if user is None:
             raise CredentialsException()
