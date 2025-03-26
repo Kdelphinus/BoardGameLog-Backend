4a27ab50df1049c8bba593ba13cc00e9e50f895d
Myoungjun Ko <kmj951015@gmail.com>
2025-03-26 21:12:10 +0900

    :art: async_session -> db_session 으로 변경

--- test/test_main.py
+++ test/test_main.py
@@ -1,12 +1,12 @@
 import pytest
-
 from httpx import AsyncClient, ASGITransport
+from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.main import app
 
 
 @pytest.mark.asyncio
-async def test_async_read_main(async_session):
+async def test_async_read_main(db_session: AsyncSession):
     async with AsyncClient(
         transport=ASGITransport(app=app), base_url="http://test"
     ) as client:
