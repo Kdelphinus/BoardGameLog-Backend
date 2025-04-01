8a98e895167704d6b3b33a055a142327b7006eb0
Myoungjun Ko <kmj951015@gmail.com>
2025-04-01 20:08:31 +0900

    :card_file_box: is_deleted와 deleted_at 속성 추가 및 name unique 속성 추가

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Boolean, DateTime
 from sqlalchemy.orm import relationship
 
 from app.db.database import Base
@@ -8,8 +8,12 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, nullable=False)  # 사용자의 이름
-    email = Column(String, unique=True, index=True, nullable=False)  # 사용자의 이메일
+    name = Column(String, unique=True, nullable=False)  # 사용자의 이름
+    email = Column(String, unique=True, nullable=False)  # 사용자의 이메일
     password = Column(String, nullable=False)  # 사용자의 비밀번호
+
+    # 추후 쿼리 효율이 떨어지면 인덱싱 고려
+    is_deleted = Column(Boolean, nullable=False, default=False)  # 탈퇴 유무
+    deleted_at = Column(DateTime, nullable=True)  # 탈퇴한 날짜
 
     played_the_games = relationship("GameLog", back_populates="user")
