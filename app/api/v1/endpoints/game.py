423cf0cbea7192d69241e36c269e29094747bf36
Myoungjun Ko <kmj951015@gmail.com>
2025-04-02 20:21:20 +0900

    :bulb: 함수 주석 추가

--- app/api/v1/endpoints/game.py
+++ app/api/v1/endpoints/game.py
@@ -14,6 +14,9 @@ router = APIRouter()
 async def create_game(game_info: GameCreate, db: AsyncSession = Depends(get_db)):
     """
     게임 정보를 생성하는 API
+    Args:
+        game_info: 생성할 게임의 정보
+        db: AsyncSession
     """
     await is_not_existing_game(db, game_name=game_info.name)
     await create_game_in_db(db, game_info)
@@ -23,6 +26,11 @@ async def create_game(game_info: GameCreate, db: AsyncSession = Depends(get_db))
 async def get_all_game(db: AsyncSession = Depends(get_db)):
     """
     등록된 모든 게임 목록을 반환하는 API
+    Args:
+        db: AsyncSession
+
+    Returns:
+        전체 게임 목록
     """
     return await get_game_in_db(db)
 
@@ -31,6 +39,12 @@ async def get_all_game(db: AsyncSession = Depends(get_db)):
 async def get_game(game_name: str, db: AsyncSession = Depends(get_db)):
     """
     주어진 게임의 정보를 반환하는 API
+    Args:
+        game_name: 불러올 게임의 이름
+        db: AsyncSession
+
+    Returns:
+        요청한 게임의 정보
     """
     game = await is_existing_game(db, game_name)
     return game
