f2cb323afe1c16e9945e92976a3af2863d6ad01a
Myoungjun Ko <kmj951015@gmail.com>
2025-04-13 20:48:36 +0900

    :recycle: game_log_info에서 빼지 않고 participant_num을 바로 받아오도록 수정

--- app/services/game_log.py
+++ app/services/game_log.py
@@ -3,10 +3,16 @@ from app.models.game import Game
 from app.schemas.game_log import GameLogCreate
 
 
-async def validate_participant_num(game: Game, game_log_info: GameLogCreate) -> None:
+async def validate_participant_num(game: Game, participant_num: int) -> None:
+    """
+    참석 인원이 유효한지 확인하는 함수
+    Args:
+        game: 진행한 게임
+        participant_num: 게임에 참여한 인원 수
+    """
     if (
-        game_log_info.participant_num < game.min_possible_num
-        or game_log_info.participant_num > game.max_possible_num
+        participant_num < game.min_possible_num
+        or participant_num > game.max_possible_num
     ):
         raise UnprocessableEntityException(
             detail="Participant_num must between min_possible_num and max_possible_num.",
