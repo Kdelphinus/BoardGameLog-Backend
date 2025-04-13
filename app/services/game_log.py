78fdfc0596b01f9abc36979ecb99a2e076da8148
Myoungjun Ko <kmj951015@gmail.com>
2025-04-13 20:52:40 +0900

    :sparkles: 현재 존재하는 게임 로그인지 확인하는 함수 추가

--- app/services/game_log.py
+++ app/services/game_log.py
@@ -1,6 +1,9 @@
-from app.core.exceptions import UnprocessableEntityException
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.core.exceptions import UnprocessableEntityException, NotFoundException
 from app.models.game import Game
-from app.schemas.game_log import GameLogCreate
+from app.models.game_log import GameLog
+from app.crud.game_log import get_game_log_in_db
 
 
 async def validate_participant_num(game: Game, participant_num: int) -> None:
@@ -17,3 +20,21 @@ async def validate_participant_num(game: Game, participant_num: int) -> None:
         raise UnprocessableEntityException(
             detail="Participant_num must between min_possible_num and max_possible_num.",
         )
+
+
+async def is_existing_game_log(db: AsyncSession, game_log_id: int) -> GameLog:
+    """
+    주어진 id가 존재하는 게임 기록의 id인지 확인하는 함수
+    Args:
+        db: AsyncSession
+        game_log_id: 찾고자 하는 게임 기록의 id
+
+    Returns:
+        있을 경우, 찾고자 하는 게임의 기록
+    """
+    game_log = await get_game_log_in_db(db=db, game_log_id=game_log_id)
+    if not game_log:
+        raise NotFoundException(
+            detail=f"Game log [{game_log_id}] is not found.",
+        )
+    return game_log
