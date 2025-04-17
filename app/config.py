faa32e0e3da112a08e5904c27e49326fa3469ba5
Myoungjun Ko <kmj951015@gmail.com>
2025-04-17 19:07:35 +0900

    :hammer: .env.test 파일 충돌이 일어나지 않도록 쉊ㅇ

--- app/config.py
+++ app/config.py
@@ -2,9 +2,10 @@ import os
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from dotenv import load_dotenv
 
-if os.getenv("TEST_ENV", "").lower() == "true":
-    ENV_FILE = ".env.test"
-elif os.getenv("DOCKER_ENV", "").lower() == "true":
+if (
+    os.getenv("DOCKER_ENV", "").lower() == "true"
+    or os.getenv("TEST_ENV", "").lower() == "true"
+):
     ENV_FILE = ".env"
 else:
     ENV_FILE = ".env.local"
